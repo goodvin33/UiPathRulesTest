6007d6a06ac2f3fd9c7e3f67825e09f970628b10
goodvin3311@gmail.com <goodvin3311@gmail.com>
2021-11-21 19:10:51 +0500

    Add project files.

---
+++ TestRulesLibrary/RulesRepository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UiPath.Studio.Activities.Api;
+using UiPath.Studio.Activities.Api.Analyzer;
+using UiPath.Studio.Activities.Api.Analyzer.Rules;
+using UiPath.Studio.Analyzer.Models;
+
+namespace TestRulesLibrary
+{
+    public class RulesRepository : IRegisterAnalyzerConfiguration
+    {
+        public void Initialize(IAnalyzerConfigurationService workflowAnalyzerConfigService)
+        {
+            if (workflowAnalyzerConfigService.HasFeature("WorkflowAnalyzerV4"))
+                return;
+            var forbiddenStringRule = new Rule<IActivityModel>("TestRule", "DE-LIB-001", InspectTestRule);
+            forbiddenStringRule.DefaultErrorLevel = System.Diagnostics.TraceLevel.Error;
+            forbiddenStringRule.Parameters.Add("test_parametrs", new Parameter()
+            {
+                DefaultValue = "testDefaultValue",
+                Key = "testParametrKey",
+                LocalizedDisplayName = "testDisplayName",
+                
+
+            });
+
+            workflowAnalyzerConfigService.AddRule<IActivityModel>(forbiddenStringRule);
+
+        }
+
+        private InspectionResult InspectTestRule(IActivityModel activityToInspect, Rule configuredRule)
+        {
+            var configuredstring = configuredRule.Parameters["testParametrKey"].Value;
+            if (string.IsNullOrWhiteSpace(configuredstring))
+            {
+                return new InspectionResult() { HasErrors = false };
+            }    
+
+            if(activityToInspect.Variables.Count == 0)
+            {
+                return new InspectionResult() { HasErrors = false };
+            }
+
+            var messageList = new List<InspectionMessage>();
+
+            foreach (var variable in activityToInspect.Variables)
+            {
+                if(variable.DisplayName.Contains(configuredstring))
+                {
+                    messageList.Add(new InspectionMessage()
+                    {
+                        Message = $"Variable {variable.DisplayName} contains an illegal string: {configuredstring}"
+                    });
+                }    
+            }
+
+            if (messageList.Count > 0)
+                return new InspectionResult()
+                {
+                    HasErrors = true,
+                    InspectionMessages = messageList,
+                    RecommendationMessage = "Fix your test project",
+                    ErrorLevel = configuredRule.ErrorLevel
+                };
+            return new InspectionResult() { HasErrors = false };
+        }
+
+    }
+}
